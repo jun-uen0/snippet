c53c7f6dfdb89b16ccd560fe19bb3f0c501500f6
jun-uen0 <hello@junueno.dev>
2022-05-31 00:23:58 +0900

    js: updated binarySearch

--- js/binarySearch.js
+++ js/binarySearch.js
@@ -1,8 +1,8 @@
 let idx = 0
 
-const compare = (arr, target) => {
+function binarySearch (arr, target) {
 
-  if(arr.length === 0) {
+  if (arr.length === 0) {
     if(arr[0] === target) {
       return idx
     } else {
@@ -12,15 +12,23 @@ const compare = (arr, target) => {
 
   const mid = Math.floor(arr.length / 2)
   const pivot = arr[mid]
-  const right = arr.slice(mid + 1)
-  const left = arr.slice(0, mid)
+
   if (pivot === target) {
     idx += mid
     return idx
-  } else if (pivot > target) {
-    return compare(left, target)
-  } else {
-    idx += mid + 1
-    return compare(right, target)
   }
+
+  if (pivot > target) {
+    const left = arr.slice(0, mid)
+    return binarySearch(left, target)
+  }
+
+  if (pivot < target) {
+    const right = arr.slice(mid + 1)
+    idx += mid + 1
+    return binarySearch(right, target)
+  }
+  return
 }
+
+console.log(binarySearch([-1,0,3,5,9,12], 9))
