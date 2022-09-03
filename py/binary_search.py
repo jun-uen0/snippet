8aa93d9f431db4562bde26e395ee0ad7dd149804
jun-uen0 <hello@junueno.dev>
2022-09-03 19:02:37 +0900

    add binary search with recursive method

--- py/binary_search.py
+++ py/binary_search.py
@@ -1,19 +1,39 @@
-def binary_search(list, item):
-  low = 0
-  high = len(list) - 1
+from re import T
 
-  while low <= high:
-    mid = (low + high) // 2
-    guess = list[mid]
-    if guess == item:
+
+def binary_search_itr(lst, target):
+  start = 0
+  end = len(lst) - 1
+
+  while start <= end:
+    mid = (start + end) // 2
+    guess = lst[mid]
+    if guess == target:
       return mid
-    if guess > item:
-      high = mid - 1
+    if guess > target:
+      end = mid - 1
     else:
-      low = mid + 1
+      start = mid + 1
   return None
+
+def binary_search_rcr(lst, target, start, end):
+	if end >= start:
+		mid = (end + start) // 2
+		if lst[mid] == target:
+			return mid
+		elif lst[mid] > target:
+			return binary_search_rcr(lst, target, start, mid - 1)
+		else:
+			return binary_search_rcr(lst, target, mid + 1, end)
+	else:
+		return None
 
 my_list = [1, 3, 5, 7, 9]
 
-print(binary_search(my_list, 3))  # 1
-print(binary_search(my_list, -1)) # None
+# Iterative
+print(binary_search_itr(my_list, 3))  # 1
+print(binary_search_itr(my_list, -1)) # None
+
+# Recursive
+print(binary_search_rcr(my_list, 9, 0, len(my_list)-1)) # 4
+print(binary_search_rcr(my_list, 10, 0, len(my_list)-1)) # None
