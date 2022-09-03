9a8fcf513de0da62fd7f83654acfaa57706b4e8e
jun-uen0 <hello@junueno.dev>
2022-09-03 19:13:58 +0900

    fix binary searches

--- py/binary_search.py
+++ py/binary_search.py
@@ -1,32 +1,30 @@
-from re import T
-
-
 def binary_search_itr(lst, target):
   start = 0
   end = len(lst) - 1
 
   while start <= end:
     mid = (start + end) // 2
-    guess = lst[mid]
-    if guess == target:
+    if lst[mid] == target:
       return mid
-    if guess > target:
+    if lst[mid] > target:
       end = mid - 1
     else:
       start = mid + 1
   return None
 
 def binary_search_rcr(lst, target, start, end):
-	if end >= start:
+
+	while start <= end:
 		mid = (end + start) // 2
 		if lst[mid] == target:
 			return mid
 		elif lst[mid] > target:
-			return binary_search_rcr(lst, target, start, mid - 1)
+			end = mid - 1
+			return binary_search_rcr(lst, target, start, end)
 		else:
-			return binary_search_rcr(lst, target, mid + 1, end)
-	else:
-		return None
+			start = mid + 1
+			return binary_search_rcr(lst, target, start, end)
+	return None
 
 my_list = [1, 3, 5, 7, 9]
 
